41c62039d3e77c9382c862d97d9b0042ef6fc8b9
M Verbeek <maikel_verbeek@hotmail.com>
2016-11-15 15:48:34 +0100

    layout detail item + landscape view + about page + menu + click on textview (item detail)

---
+++ app/src/main/java/nl/fressh/nederlandviertfeest/AboutActivity.java
@@ -0,0 +1,21 @@
+package nl.fressh.nederlandviertfeest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Maikel on 15-11-16.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+    }
+
+}
--- app/src/main/java/nl/fressh/nederlandviertfeest/ListDetailActivity.java
+++ app/src/main/java/nl/fressh/nederlandviertfeest/ListDetailActivity.java
@@ -1,21 +1,53 @@
 package nl.fressh.nederlandviertfeest;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListDetailActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_item_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        String dummyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+        "Ut volutpat interdum interdum. Nulla laoreet lacus diam, vitae " +
+                "sodales sapien commodo faucibus. Vestibulum et feugiat enim. Donec " +
+                "semper mi et euismod tempor. Sed sodales eleifend mi id varius. Nam " +
+                "et ornare enim, sit amet gravida sapien. Quisque gravida et enim vel " +
+                "volutpat. Vivamus egestas ut felis a blandit. Vivamus fringilla " +
+                "dignissim mollis. Maecenas imperdiet interdum hendrerit. Aliquam" +
+                " dictum hendrerit ultrices. Ut vitae vestibulum dolor. Donec auctor ante" +
+                " eget libero molestie porta. Nam tempor fringilla ultricies. Nam sem " +
+                "lectus, feugiat eget ullamcorper vitae, ornare et sem. Fusce dapibus ipsum" +
+                " sed laoreet suscipit.";
+
+        TextView info = (TextView)findViewById(R.id.textView2);
+        info.setText(dummyText);
+
+        info.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
     }
+
+
 
 }
